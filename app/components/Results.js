9e69f80d4784523a34dfdb7a8bb612d161106499
Elyager <elyager@gmail.com>
2016-06-23 12:43:15 -0700

    Add MainContainer component and StarOver private component

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,9 +4,16 @@ var styles = require('../styles');
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
+var MainContainer = require('../components/MainContainer');
 
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, 2, ' ')}</pre>
+function StarOver() {
+  return (
+    <div className='col-sm-12' style={styles.space}>
+      <Link to='playerOne'>
+        <button type='button' className='btn btn-lg btn-danger'>Star Over</button>
+      </Link>
+    </div>
+  )
 }
 
 function Results(props) {
@@ -17,18 +24,16 @@ function Results(props) {
   }
   if (props.scores[0] === props.scores[1]) {
     return (
-      <div className='col-sm-12' style={styles.space}>
+      <div className='jumbotron col-sm-12 text-center' style={styles.space}>
         <h1>It is a tie!</h1>
-        <Link to='playerOne'>
-          <button type='button' className='btn btn-lg btn-danger'>Star Over</button>
-        </Link>
+        <StarOver />
       </div>
     )
   }
   var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
   var loserIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
+    <MainContainer>
       <h1>Results</h1>
       <div className='col-sm-8 col-sm-offset-2'>
         <UserDetailsWrapper header='Winner'>
@@ -38,12 +43,8 @@ function Results(props) {
           <UserDetails score={props.scores[loserIndex]} info={props.playersInfo[loserIndex]} />
         </UserDetailsWrapper>
       </div>
-      <div className='col-sm-12' style={styles.space}>
-        <Link to='playerOne'>
-          <button type='button' className='btn btn-lg btn-danger'>Star Over</button>
-        </Link>
-      </div>
-    </div>
+      <StarOver />
+    </MainContainer>
   )
 }
 
