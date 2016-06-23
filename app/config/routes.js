a3146609c71f32b20fe1e213e3863b305e6c2ecb
Elyager <elyager@gmail.com>
2016-06-22 20:13:31 -0700

    Separate business layer and persentation layer Use stateless functional components

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,6 +4,7 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
+var PromptContainer = require('../containers/PromptContainer');
 
 //Routes maps a url to a components
 //components
@@ -14,6 +15,8 @@ var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two'  component={PromptContainer} />
     </Route>
   </Router>
 );
