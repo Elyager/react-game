2d1166b4b93910f2a1d995b441dbc8b49abf5fb1
Elyager <elyager@gmail.com>
2016-06-22 20:39:51 -0700

    Add battle route and review React's components lifecycle

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,6 +5,7 @@ var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var PromptContainer = require('../containers/PromptContainer');
+var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
 
 //Routes maps a url to a components
 //components
@@ -17,6 +18,7 @@ var routes = (
       <IndexRoute component={Home} />
       <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' header='Player Two'  component={PromptContainer} />
+      <Route path='battle' component={ConfirmBattleContainer} />
     </Route>
   </Router>
 );
