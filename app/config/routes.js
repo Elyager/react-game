c826ed349b1018c752bc9ed256136af763f1d5fe
Elyager <elyager@gmail.com>
2016-06-23 11:07:42 -0700

    Add Results component and fix playersInfo typo

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,6 +6,7 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var PromptContainer = require('../containers/PromptContainer');
 var ConfirmBattleContainer = require('../containers/ConfirmBattleContainer');
+var ResultsContainer = require('../containers/ResultsContainer');
 
 //Routes maps a url to a components
 //components
@@ -19,6 +20,7 @@ var routes = (
       <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' header='Player Two'  component={PromptContainer} />
       <Route path='battle' component={ConfirmBattleContainer} />
+      <Route path='results' component={ResultsContainer} />
     </Route>
   </Router>
 );
